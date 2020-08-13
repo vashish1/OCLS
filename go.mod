02fea46de2399479feef7c2303bae9ebabef587c
vashish1 <vashishtiv@gmail.com>
2020-08-13 20:08:51 +0530

    refactor

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/vashish1/OnlineClassPortal
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gofiber/cors v0.2.2
+	github.com/gofiber/fiber v1.13.3
+	github.com/gorilla/mux v1.7.4
+	go.mongodb.org/mongo-driver v1.4.0
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de
+	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	google.golang.org/api v0.30.0
+)
---
+++ pkg/database/student/student.go
@@ -0,0 +1,43 @@
+package student
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/vashish1/OnlineClassPortal/pkg/database"
+	"github.com/vashish1/OnlineClassPortal/pkg/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+var db = database.StudentDb()
+
+func Exist(email string) (models.Student, bool) {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	var data models.Student
+	filter := bson.D{
+		{"email", email},
+		{"freeze", false},
+	}
+	err := db.FindOne(ctx, filter).Decode(&data)
+	if err != nil {
+		return data, false
+	}
+	return data, true
+}
+
+func IsAvailable(uid string) bool {
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	filter := bson.D{
+		{"uid", uid},
+		{"freeze", false},
+	}
+	err := db.FindOne(ctx, filter)
+	if err.Err() == nil {
+		fmt.Println("Same Uid exists", err)
+		return true
+	}
+	return false
+}
