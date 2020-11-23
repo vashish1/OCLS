77b2e6f038d689484cb1c802fccbcf755d1102ad
vashish1 <vashishtiv@gmail.com>
2020-11-23 23:27:25 +0530

    added structures

--- pkg/hub/hub.go
+++ pkg/hub/hub.go
@@ -1,266 +1,264 @@
-// package model
+package model
 
-// import (
-// 	"encoding/json"
-// 	"net/http"
-// 	"sync"
-// 	"time"
+import (
+	"encoding/json"
+	"net/http"
+	"sync"
+	"time"
 
-// 	"github.com/fasthttp/websocket"
+	"github.com/fasthttp/websocket"
+)
 
-// 	"github.com/metaclips/LetsTalk/backend/values"
-// )
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 30 * time.Second
 
-// const (
-// 	// Time allowed to write a message to the peer.
-// 	writeWait = 30 * time.Second
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 120 * time.Second
 
-// 	// Time allowed to read the next pong message from the peer.
-// 	pongWait = 120 * time.Second
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+)
 
-// 	// Send pings to peer with this period. Must be less than pongWait.
-// 	pingPeriod = (pongWait * 9) / 10
-// )
+var Upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
-// var Upgrader = websocket.Upgrader{
-// 	ReadBufferSize:  1024,
-// 	WriteBufferSize: 1024,
-// }
+var HubConstruct = hub{
+	broadcast:  make(chan wsMessage),
+	register:   make(chan subscription),
+	unRegister: make(chan subscription),
+	users: wsUsers{
+		users: make(map[string]map[*connection]bool),
+		mutex: &sync.RWMutex{},
+	},
+}
 
-// var HubConstruct = hub{
-// 	broadcast:  make(chan wsMessage),
-// 	register:   make(chan subscription),
-// 	unRegister: make(chan subscription),
-// 	users: wsUsers{
-// 		users: make(map[string]map[*connection]bool),
-// 		mutex: &sync.RWMutex{},
-// 	},
-// }
+func (h *hub) RegisterWS(ws *websocket.Conn, email string) {
+	c := &connection{send: make(chan []byte, 256), ws: ws}
 
-// func (h *hub) RegisterWS(ws *websocket.Conn, email string) {
-// 	c := &connection{send: make(chan []byte, 256), ws: ws}
+	s := subscription{conn: c, user: email}
+	HubConstruct.register <- s
+	log.Infoln("user", email, "Connected")
+	go s.readPump(email)
+	go s.writePump()
 
-// 	s := subscription{conn: c, user: email}
-// 	HubConstruct.register <- s
-// 	log.Infoln("user", email, "Connected")
-// 	go s.readPump(email)
-// 	go s.writePump()
+}
 
-// }
+func (h *hub) Run() {
+	Upgrader.CheckOrigin = func(r *http.Request) bool {
+		host := r.Header.Get("Origin")
 
-// func (h *hub) Run() {
-// 	Upgrader.CheckOrigin = func(r *http.Request) bool {
-// 		host := r.Header.Get("Origin")
+		for _, allowdOrigin := range values.Config.CorsAllowedOrigins {
+			if host == allowdOrigin {
+				return true
+			}
+		}
+		log.Debugln("Host is not allowed:", host)
 
-// 		for _, allowdOrigin := range values.Config.CorsAllowedOrigins {
-// 			if host == allowdOrigin {
-// 				return true
-// 			}
-// 		}
-// 		log.Debugln("Host is not allowed:", host)
+		return false
+	}
 
-// 		return false
-// 	}
+	for {
+		select {
+		case s := <-h.register:
+			h.users.mutex.Lock()
 
-// 	for {
-// 		select {
-// 		case s := <-h.register:
-// 			h.users.mutex.Lock()
+			if _, exists := h.users.users[s.user]; !exists {
+				connections := make(map[*connection]bool)
 
-// 			if _, exists := h.users.users[s.user]; !exists {
-// 				connections := make(map[*connection]bool)
+				h.users.users[s.user] = connections
+			}
 
-// 				h.users.users[s.user] = connections
-// 			}
+			h.users.users[s.user][s.conn] = true
 
-// 			h.users.users[s.user][s.conn] = true
+			log.Infoln(s.user, "registered")
+			go broadcastOnlineStatusToAllUserRoom(s.user, true)
+			h.users.mutex.Unlock()
 
-// 			log.Infoln(s.user, "registered")
-// 			go broadcastOnlineStatusToAllUserRoom(s.user, true)
-// 			h.users.mutex.Unlock()
+		case s := <-h.unRegister:
+			h.users.mutex.Lock()
+			connections, exists := h.users.users[s.user]
+			if exists {
+				if _, ok := connections[s.conn]; ok {
+					delete(connections, s.conn)
+					close(s.conn.send)
+					if len(connections) == 0 {
+						delete(h.users.users, s.user)
+						go broadcastOnlineStatusToAllUserRoom(s.user, false)
+						log.Infoln(s.user, "offline")
+					}
+					log.Infoln(s.user, "subscription removed")
+				}
+			}
+			h.users.mutex.Unlock()
 
-// 		case s := <-h.unRegister:
-// 			h.users.mutex.Lock()
-// 			connections, exists := h.users.users[s.user]
-// 			if exists {
-// 				if _, ok := connections[s.conn]; ok {
-// 					delete(connections, s.conn)
-// 					close(s.conn.send)
-// 					if len(connections) == 0 {
-// 						delete(h.users.users, s.user)
-// 						go broadcastOnlineStatusToAllUserRoom(s.user, false)
-// 						log.Infoln(s.user, "offline")
-// 					}
-// 					log.Infoln(s.user, "subscription removed")
-// 				}
-// 			}
-// 			h.users.mutex.Unlock()
+		case m := <-h.broadcast:
+			h.users.mutex.RLock()
+			connections := h.users.users[m.user]
+			for c := range connections {
+				select {
+				case c.send <- m.data:
+				default:
+					close(c.send)
+					delete(connections, c)
+					if len(connections) == 0 {
+						h.users.mutex.Lock()
+						delete(h.users.users, m.user)
+						h.users.mutex.Unlock()
+						go broadcastOnlineStatusToAllUserRoom(m.user, false)
+					}
+				}
+			}
+			h.users.mutex.RUnlock()
+		}
+	}
+}
 
-// 		case m := <-h.broadcast:
-// 			h.users.mutex.RLock()
-// 			connections := h.users.users[m.user]
-// 			for c := range connections {
-// 				select {
-// 				case c.send <- m.data:
-// 				default:
-// 					close(c.send)
-// 					delete(connections, c)
-// 					if len(connections) == 0 {
-// 						h.users.mutex.Lock()
-// 						delete(h.users.users, m.user)
-// 						h.users.mutex.Unlock()
-// 						go broadcastOnlineStatusToAllUserRoom(m.user, false)
-// 					}
-// 				}
-// 			}
-// 			h.users.mutex.RUnlock()
-// 		}
-// 	}
-// }
+func (h *hub) sendMessage(msg []byte, user string) {
+	m := wsMessage{msg, user}
+	HubConstruct.broadcast <- m
+}
 
-// func (h *hub) sendMessage(msg []byte, user string) {
-// 	m := wsMessage{msg, user}
-// 	HubConstruct.broadcast <- m
-// }
+// WritePump pumps messages from the hub to the websocket connection.
+func (s *subscription) writePump() {
+	c := s.conn
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
 
-// // WritePump pumps messages from the hub to the websocket connection.
-// func (s *subscription) writePump() {
-// 	c := s.conn
-// 	ticker := time.NewTicker(pingPeriod)
-// 	defer func() {
-// 		ticker.Stop()
-// 		c.ws.Close()
-// 	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.write(websocket.CloseMessage, []byte{})
+				return
+			}
 
-// 	for {
-// 		select {
-// 		case message, ok := <-c.send:
-// 			if !ok {
-// 				c.write(websocket.CloseMessage, []byte{})
-// 				return
-// 			}
+			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
 
-// 			if err := c.write(websocket.TextMessage, message); err != nil {
-// 				return
-// 			}
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
+		}
+	}
+}
 
-// 		case <-ticker.C:
-// 			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
-// 				return
-// 			}
-// 		}
-// 	}
-// }
+// write writes a message with the given message type and payload.
+func (c *connection) write(mt int, payload []byte) error {
+	if err := c.ws.SetWriteDeadline(time.Now().Add(writeWait)); err != nil {
+		return err
+	}
 
-// // write writes a message with the given message type and payload.
-// func (c *connection) write(mt int, payload []byte) error {
-// 	if err := c.ws.SetWriteDeadline(time.Now().Add(writeWait)); err != nil {
-// 		return err
-// 	}
+	return c.ws.WriteMessage(mt, payload)
+}
 
-// 	return c.ws.WriteMessage(mt, payload)
-// }
+// ReadPump pumps messages from the websocket connection to the hub.
+func (s subscription) readPump(user string) {
+	c := s.conn
 
-// // ReadPump pumps messages from the websocket connection to the hub.
-// func (s subscription) readPump(user string) {
-// 	c := s.conn
+	defer func() {
+		HubConstruct.unRegister <- s
+		c.ws.Close()
+	}()
 
-// 	defer func() {
-// 		HubConstruct.unRegister <- s
-// 		c.ws.Close()
-// 	}()
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
 
-// 	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(
+		func(string) error {
+			return c.ws.SetReadDeadline(time.Now().Add(pongWait))
+		})
 
-// 	c.ws.SetPongHandler(
-// 		func(string) error {
-// 			return c.ws.SetReadDeadline(time.Now().Add(pongWait))
-// 		})
+	for {
+		var err error
+		var msg messageBytes
+		_, msg, err = c.ws.ReadMessage()
 
-// 	for {
-// 		var err error
-// 		var msg messageBytes
-// 		_, msg, err = c.ws.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+				log.Errorln("error: ", err)
+			}
 
-// 		if err != nil {
-// 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-// 				log.Errorln("error: ", err)
-// 			}
+			return
+		}
 
-// 			return
-// 		}
+		data := struct {
+			MsgType    string `json:"msgType"`
+			User       string `json:"userID"`
+			SearchText string `json:"searchText"`
+		}{}
 
-// 		data := struct {
-// 			MsgType    string `json:"msgType"`
-// 			User       string `json:"userID"`
-// 			SearchText string `json:"searchText"`
-// 		}{}
+		err = json.Unmarshal(msg, &data)
+		if err != nil {
+			log.Println("could not unmarshal json")
+		}
 
-// 		err = json.Unmarshal(msg, &data)
-// 		if err != nil {
-// 			log.Println("could not unmarshal json")
-// 		}
+		if data.User != user {
+			log.Warningf("an invalidated user tried to make websocket request for %s, invalidated user: %s, user: %s", data.MsgType, data.User, user)
+			continue
+		}
 
-// 		if data.User != user {
-// 			log.Warningf("an invalidated user tried to make websocket request for %s, invalidated user: %s, user: %s", data.MsgType, data.User, user)
-// 			continue
-// 		}
+		switch data.MsgType {
+		// TODO: add support to remove message.
+		case values.WebsocketOpenMsgType:
+			handleLoadUserContent(user)
 
-// 		switch data.MsgType {
-// 		// TODO: add support to remove message.
-// 		case values.WebsocketOpenMsgType:
-// 			handleLoadUserContent(user)
+		case values.RequestMessages:
+			msg.handleRequestMessages(user)
 
-// 		case values.RequestMessages:
-// 			msg.handleRequestMessages(user)
+		case values.NewMessageMsgType:
+			msg.handleNewMessage()
 
-// 		case values.NewMessageMsgType:
-// 			msg.handleNewMessage()
+		case values.CreateRoomMsgType:
+			msg.handleCreateNewRoom()
 
-// 		case values.CreateRoomMsgType:
-// 			msg.handleCreateNewRoom()
+		case values.JoinRoomMsgType:
+			msg.handleUserAcceptRoomRequest()
 
-// 		case values.JoinRoomMsgType:
-// 			msg.handleUserAcceptRoomRequest()
+		case values.ExitRoomMsgType:
+			msg.handleExitRoom(user)
 
-// 		case values.ExitRoomMsgType:
-// 			msg.handleExitRoom(user)
+		case values.RequestUsersToJoinRoomMsgType:
+			msg.handleRequestUserToJoinRoom()
 
-// 		case values.RequestUsersToJoinRoomMsgType:
-// 			msg.handleRequestUserToJoinRoom()
+		case values.NewFileUploadMsgType:
+			msg.handleNewFileUpload()
 
-// 		case values.NewFileUploadMsgType:
-// 			msg.handleNewFileUpload()
+		case values.UploadFileChunkMsgType:
+			msg.handleUploadFileChunk()
 
-// 		case values.UploadFileChunkMsgType:
-// 			msg.handleUploadFileChunk()
+		case values.UploadFileSuccessMsgType:
+			msg.handleUploadFileUploadComplete()
 
-// 		case values.UploadFileSuccessMsgType:
-// 			msg.handleUploadFileUploadComplete()
+		case values.RequestDownloadMsgType:
+			msg.handleRequestDownload(user)
 
-// 		case values.RequestDownloadMsgType:
-// 			msg.handleRequestDownload(user)
+		case values.DownloadFileChunkMsgType:
+			msg.handleFileDownload(user)
 
-// 		case values.DownloadFileChunkMsgType:
-// 			msg.handleFileDownload(user)
+		case values.StartClassSession:
+			classSessions.startClassSession(msg, user)
 
-// 		case values.StartClassSession:
-// 			classSessions.startClassSession(msg, user)
+		case values.JoinClassSession:
+			classSessions.joinClassSession(msg, user)
 
-// 		case values.JoinClassSession:
-// 			classSessions.joinClassSession(msg, user)
+		case values.EndClassSession:
+			classSessions.endClassSession(user)
 
-// 		case values.EndClassSession:
-// 			classSessions.endClassSession(user)
+		case values.RenegotiateSDP:
+			sdpConstruct{}.acceptRenegotiation(msg)
 
-// 		case values.RenegotiateSDP:
-// 			sdpConstruct{}.acceptRenegotiation(msg)
+		case values.SearchUserMsgType:
+			handleSearchUser(data.SearchText, user)
 
-// 		case values.SearchUserMsgType:
-// 			handleSearchUser(data.SearchText, user)
-
-// 		default:
-// 			log.Println("Could not convert required type", data.MsgType)
-// 		}
-// 	}
-// }
+		default:
+			log.Println("Could not convert required type", data.MsgType)
+		}
+	}
+}
---
+++ pkg/models/hub.go
@@ -0,0 +1,223 @@
+package models
+
+import (
+	"sync"
+	"time"
+
+	"github.com/fasthttp/websocket"
+	"github.com/pion/webrtc/v2"
+)
+
+type Hub struct {
+}
+
+type CookieDetail struct {
+	Email      string
+	Collection string
+	CookieName string
+	Path       string
+	Data       CookieData
+}
+
+type CookieData struct {
+	ExitTime time.Time
+	UUID     string
+	Email    string
+}
+
+type User struct {
+	Email string `bson:"_id" json:"userID"`
+	Name  string `bson:"name" json:"name"`
+	DOB   string `bson:"age" json:"age"`
+	Class string `bson:"class" json:"class"`
+	// ID should either be users matric or leading email stripping @....
+	Password         []byte        `bson:"password" json:"-"`
+	PasswordInString string        `bson:"-" json:"password"`
+	RoomsJoined      []roomsJoined `bson:"roomsJoined" json:"roomsJoined"`
+	JoinRequest      []joinRequest `bson:"joinRequest" json:"joinRequest"`
+	UUID             string        `bson:"loginUUID" json:"uuid"`
+}
+
+type roomsJoined struct {
+	RoomID   string `bson:"roomID" json:"roomID"`
+	RoomName string `bson:"roomName" json:"roomName"`
+}
+
+type joinRequest struct {
+	RoomID             string   `bson:"_id" json:"roomID"`
+	RoomName           string   `bson:"roomName" json:"roomName"`
+	RequestingUserName string   `bson:"requestingUserName" json:"requestingUserName"`
+	RequestingUserID   string   `bson:"requestingUserID" json:"requestingUserID"`
+	Users              []string `bson:"-" json:"users"` // Users whom join requests are to be sent to, if client wants to send request to multiple users.
+}
+
+// Room struct defineds content details for users room.
+// Message count is used to track amount of messages sent by users in room,
+// this helps with partitioning messages on retrieval, messages are retrieved in 20s on request.
+// FirstLoad is specified if user initially wants to retrieve room messages from frontend.
+type room struct {
+	RoomID          string    `bson:"_id" json:"roomID,omitempty"`
+	RoomName        string    `bson:"roomName" json:"roomName,omitempty"`
+	RoomIcon        string    `bson:"roomIcon" json:"roomIcon"`
+	RegisteredUsers []string  `bson:"registeredUsers" json:"registeredUsers,omitempty"`
+	MessageCount    int       `bson:"messageCount" json:"messageCount,omitempty"`
+	Messages        []Message `bson:"-" json:"messages,omitempty"`
+	FirstLoad       bool      `bson:"-" json:"firstLoad,omitempty"`
+}
+
+type associateStatus struct {
+	Name     string `json:"name"`
+	IsOnline bool   `json:"isOnline"`
+}
+
+// Message struct defines user message contents, size and hash is defined if user is sending files.
+// Index is used to track message count as to Rooms messages, this should help with partitioning if we
+// are to retrieve message of a particular count.
+type Message struct {
+	RoomID      string `bson:"roomID" json:"roomID,omitempty"`
+	Message     string `bson:"message" json:"message,omitempty"`
+	UserID      string `bson:"userID" json:"userID,omitempty"`
+	Name        string `bson:"name" json:"name,omitempty"`
+	Time        string `bson:"time" json:"time,omitempty"`
+	Type        string `bson:"type" json:"type,omitempty"`
+	Size        string `bson:"size,omitempty" json:"size,omitempty"`
+	Hash        string `bson:"hash,omitempty" json:"hash,omitempty"`
+	Index       int    `bson:"index" json:"index,omitempty"`
+	MessageType string `bson:"-" json:"msgType,omitempty"`
+}
+
+type joined struct {
+	RoomID      string `json:"roomID"`
+	RoomName    string `json:"roomName"`
+	Email       string `json:"userID"`
+	Name        string `json:"name"`
+	RequesterID string `json:"requesterID"`
+	Joined      bool   `json:"joined"`
+	MessageType string `bson:"-" json:"msgType"`
+}
+
+type newRoomRequest struct {
+	Email       string `json:"userID"`
+	RoomName    string `json:"roomName"`
+	MessageType string `bson:"-" json:"msgType"`
+}
+
+// File save files making sure they are distinct.
+type file struct {
+	MsgType        string `bson:"-" json:"msgType,omitempty"`
+	UniqueFileHash string `bson:"_id" json:"fileHash"`
+	FileName       string `bson:"fileName" json:"fileName"`
+	User           string `bson:"userID" json:"userID"`
+	FileSize       string `bson:"fileSize" json:"fileSize"`
+	FileType       string `bson:"fileType" json:"fileType"`
+	Chunks         int    `bson:"chunks,omitempty" json:"chunks"`
+}
+
+type fileChunks struct {
+	MsgType            string `bson:"-" json:"msgType,omitempty"`
+	FileName           string `bson:"-" json:"fileName,omitempty"`
+	UniqueFileHash     string `bson:"_id" json:"fileHash,omitempty"`
+	CompressedFileHash string `bson:"compressedFileHash" json:"compressedFileHash,omitempty"`
+	FileBinary         string `bson:"fileChunk" json:"fileChunk,omitempty"`
+	ChunkIndex         int    `bson:"chunkIndex" json:"chunkIndex"`
+}
+
+type wsMessage struct {
+	data []byte
+	user string
+}
+
+// Connection is an middleman between the websocket connection and the hub.
+type connection struct {
+	ws *websocket.Conn
+
+	send chan []byte
+}
+
+type subscription struct {
+	conn *connection
+	user string
+}
+
+// Hub maintains the set of active connections and broadcasts messages to the
+// connections.
+type hub struct {
+	// Registered connections.
+	users wsUsers
+
+	// Inbound messages from the connections.
+	broadcast chan wsMessage
+
+	// Register requests from the connections.
+	register chan subscription
+
+	// Unregister requests from connections.
+	unRegister chan subscription
+}
+
+type wsUsers struct {
+	users map[string]map[*connection]bool
+	mutex *sync.RWMutex
+}
+
+// classSessionPeerConnections allows class session video calls where a publisher
+// broadcasts audio and video to other subscribers. Other subscribers broadcasts
+// audio to each other.
+// Video is mapped to unique ID which is to be generated by server for current class session
+// to video and audio track.
+// If UUID to current VIDEO track is nil. It indicates the room session is over.
+// If publisher logs off, all peer connections related to that room is closed.
+// Mutexes is integrated with video, audio and peerconnections to ensure data race free.
+type classSessionPeerConnections struct {
+	api *webrtc.API
+
+	publisherVideoTracks  map[string]*webrtc.Track // mapped sessionID to track
+	publisherTrackMutexes *sync.RWMutex
+
+	audioTrack        map[string]*webrtc.Track // mapped userID to track
+	audioTrackSender  map[*webrtc.Track][]rtpSenderData
+	audioTrackMutexes *sync.RWMutex
+
+	peerConnection        map[string]*webrtc.PeerConnection // peerConnection is mapped user to peerconnection.
+	peerConnectionMutexes *sync.RWMutex
+
+	connectedUsers      map[string][]string // publisher is mapped sessionID to all connected users.
+	connectedUsersMutex *sync.RWMutex
+}
+
+// rtpSenderData saves user RTPSender.
+// On remove track, users can easily map all audio track to its senders.
+// userID maps sender to it's peerconnection.
+type rtpSenderData struct {
+	userID string
+	sender *webrtc.RTPSender
+}
+
+// type webmWriter struct {
+// 	fileName                       string
+// 	audioWriter, videoWriter       webm.BlockWriteCloser
+// 	audioBuilder, videoBuilder     *samplebuilder.SampleBuilder
+// 	audioTimestamp, videoTimestamp uint32
+// }
+
+type sdpConstruct struct {
+	MsgType        string `json:"msgType"`
+	ClassSessionID string `json:"sessionID"`
+	AuthorName     string `json:"name"`
+	PublisherID    string `json:"publisherID"`
+	UserID         string `json:"userID"`
+	RoomID         string `json:"roomID"`
+	SDP            string `json:"sdp"`
+
+	peerConnection *webrtc.PeerConnection
+}
+
+// type dropboxUploader struct {
+// 	uploadClient       files.Client
+// 	sharableLinkClient sharing.Client
+// 	fileUploadInfo     *files.CommitInfo
+
+// 	file         *os.File
+// 	fileFullPath string
+// 	fileSize     int64
+// }
