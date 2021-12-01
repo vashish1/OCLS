8099eb6ae13bde6a05bed19d64ab84641629f2a3
vashish1 <vashishtiv@gmail.com>
2021-12-01 13:50:49 +0530

    added private route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,9 @@ import './App.css';
 import Home from './pages/Home';
 // import SignInSignUp from './pages/SignInSignUp';
 import NotFound from './pages/NotFound';
-import CreateClass from './components/createClass';
 import SignUp from './components/SignUp';
 import Login from './components/login';
 import Dashboard from './pages/Dashboard';
-import MiniDrawer from './components/Modules/NavBar';
 import AnnouncementMiniDrawer from './components/Modules/ClassAnnouncementNavBar';
 import AssignmentMiniDrawer from './components/Modules/ClassAssignmentNavBar';
 import Profile from './components/Profile';
@@ -16,27 +14,38 @@ import UpdateProfile from './components/Modules/UpdateProfile';
 import Createquiz from './quizapp/Createquiz';
 import TakeQuiz from './quizapp/TakeQuiz';
 import SignUpWithGoogle from './components/SignUpWithGoogle';
+import Private from './pages/Private';
 function App() {
   return (
     <div>
     <Router>
     
       <Routes>
-        
+      <Route exact path='/dashboard' element={<Private/>}>
         <Route exact path="/dashboard" element={<Dashboard/>} />
+        </Route>
+        <Route exact path='/profile' element={<Private/>}>
         <Route exact path="/profile" element={<Profile/>} />
+        </Route>
+        <Route exact path='/class/:id/assignment/createquiz' element={<Private/>}>
         <Route exact path="/class/:id/assignment/createquiz" element={<Createquiz/>} />
+        </Route>
+        <Route exact path='/class/:id/assignment/takequiz' element={<Private/>}>
         <Route exact path="/class/:id/assignment/takequiz" element={<TakeQuiz/>} />
-
+        </Route>
+        <Route exact path='/profile/update' element={<Private/>}>
         <Route exact path="/profile/update" element={<UpdateProfile/>} />
+        </Route>
         <Route exact path="/type" element={<SignUpWithGoogle/>} />
-      <Route exact path="/dashboard/:page?" render={props => <MiniDrawer {...props} />} />
+      
         <Route exact path="/signup" element={<SignUp/>} />
+        <Route exact path='/class/:id/announcement' element={<Private/>}>
         <Route exact path="/class/:id/announcement" element={<AnnouncementMiniDrawer/>} />
+        </Route>
+        <Route exact path='/class/:id/assignment' element={<Private/>}>
         <Route exact path="/class/:id/assignment" element={<AssignmentMiniDrawer/>} />
+        </Route>
         <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/createclass" element={<CreateClass/>} />
-        <Route exact path="/joinclass" element={<CreateClass/>} />
         <Route exact path="/" element={<Home/>} />
         <Route element={<NotFound/>} />
       </Routes>
--- frontend/src/pages/Private.js
+++ frontend/src/pages/Private.js
@@ -1,19 +1,12 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import Dashboard from "./Dashboard";
-export default function PrivateRoute({ component: Component, ...rest }) {
-  const { isLoggedIn } = useLogin();
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        return isLoggedIn ? (
-          <Component {...props} />
-        ) : (
-          <Route exact path="/" component={Dashboard} />
-        );
-      }}
-    ></Route>
-  );
+const Private = () => {
+    const auth = localStorage.getItem('token'); // determine if authorized, from context or however you're doing it
+
+    // If authorized, return an outlet that will render child elements
+    // If not, return element that will navigate to login page
+    return auth ? <Outlet /> : <Navigate to="/login" />;
 }
+
+export default Private
